6f0525ddd1556c4619d549a6ae95076b1595f51c
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2025-01-14 22:00:47 -0300

    Connexion DB

--- Personal-Expenses/src/main/java/com/gabrielle/Personal_Expenses/entity/userEntity.java
+++ Personal-Expenses/src/main/java/com/gabrielle/Personal_Expenses/entity/userEntity.java
@@ -3,6 +3,8 @@ package com.gabrielle.Personal_Expenses.entity;
 import com.gabrielle.Personal_Expenses.dto.userDTO;
 import com.gabrielle.Personal_Expenses.entity.enums.situacionUser;
 
+import java.util.Objects;
+
 import org.springframework.beans.BeanUtils;
 
 import jakarta.persistence.Column;
@@ -15,7 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "NPL_USER")
+@Table
 public class userEntity {
     
     @Id
@@ -42,5 +44,68 @@ public class userEntity {
         BeanUtils.copyProperties(user, this);
     }
 
-    public userEntity() {}
+    public userEntity() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public situacionUser situacion() {
+        return situacion;
+    }
+
+    public void setSituacion(situacionUser situacion) {
+        this.situacion = situacion;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+
+        userEntity other = (userEntity) obj;
+        return Objects.equals(id, other.id);
+    }
 }
