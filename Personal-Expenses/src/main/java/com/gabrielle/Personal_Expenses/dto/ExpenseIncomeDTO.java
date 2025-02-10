44cc18eaafed8d43ee03b3595f94edae570b7336
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2025-02-10 20:53:00 -0300

    Cadastro das despesas pessoais

---
+++ Personal-Expenses/src/main/java/com/gabrielle/Personal_Expenses/dto/ExpenseIncomeDTO.java
@@ -0,0 +1,29 @@
+package com.gabrielle.Personal_Expenses.dto;
+
+public class ExpenseIncomeDTO {
+    
+    private String date;
+    private String description;
+    private double amount;
+    private String type;
+    
+    public ExpenseIncomeDTO(String date, String description, double amount, String type) {
+        this.date = date;
+        this.description = description;
+        this.amount = amount;
+        this.type = type;
+    }
+    
+    public String getDate() {
+        return date;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public String getType() {
+        return type;
+    }
+}
