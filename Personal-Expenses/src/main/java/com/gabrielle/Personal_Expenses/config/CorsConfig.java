234837289ba1a12c87a1574c63f77d980c1248bf
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2025-02-25 21:42:23 -0300

    Conectando ao front-end

---
+++ Personal-Expenses/src/main/java/com/gabrielle/Personal_Expenses/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.gabrielle.Personal_Expenses.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+                registry.addMapping("/**") 
+                        .allowedOrigins("http://localhost:5173/") 
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
